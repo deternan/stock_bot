4c8bca8ef6557301fb850960330356e88f9be0e9
deternan <chke@iir.csie.ncku.edu.tw>
2017-07-29 18:06:47 +0800

    no message

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -30,6 +30,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.net.URL;
 import java.util.Vector;
 import java.util.regex.Matcher;
@@ -55,8 +56,6 @@ public class EchoApplication
 	
 	static ArrayList<String> code = new ArrayList<String>();
 	static ArrayList<String> name = new ArrayList<String>();
-	//Vector code = new Vector();
-	//Vector name = new Vector();
 	
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
